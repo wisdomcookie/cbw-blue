d107a38326ec05a282f17823e17547830190182a
Nathan_Dinh <nathandinht@gmail.com>
2024-11-03 21:15:54 -0500

    Add remaining links to navbar and placeholder templates, start shops pages

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-
+    path('programs/', views.programs, name='programs'),
+    path('shops/<str:shopname>/', views.shop, name='shops'),
+    path('about/mission/', views.mission, name='mission'),
+    path('about/people/', views.people, name='people'),
+    path('about/hiring/', views.hiring, name='hiring'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,3 +14,31 @@ def about(request):
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+def programs(request):
+    return render(request, 'main/programs.html')
+
+def shop(request, shopname):
+
+    shop_templates = {
+        'allentown-central': 'main/shops/allentown-central.html',
+        'allentown-east': 'main/shops/allentown-east.html',
+        'easton': 'main/shops/easton.html',
+    }
+
+    # Get the template for the given shopname, or raise a 404 if not found
+    template = shop_templates.get(shopname)
+    if template is None:
+        raise Http404("Shop not found")
+
+    return render(request, template)
+
+
+def mission(request):
+    return render(request, 'main/about/mission.html')
+
+def people(request):
+    return render(request, 'main/about/people.html')
+
+def hiring(request):
+    return render(request, 'main/about/hiring.html')
