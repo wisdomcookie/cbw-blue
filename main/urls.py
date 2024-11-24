649836deffd8c6c495c2b370c8e0b680b0b4b568
Nathan_Dinh <nathandinht@gmail.com>
2024-11-23 19:00:26 -0500

    Large set of final changes
    Redo linking to make more sense and clarify shops as different locations of same org
    Add donation section
    Link to each shop's programs from programs page
    Add volunteer page
    Standardize some info cards with different amounts of text to be same size

--- main/urls.py
+++ main/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('programs/', views.programs, name='programs'),
-    path('shops/<str:shopname>/', views.shop, name='shops'),
+    path('join/volunteer/', views.volunteer, name='volunteer'),
+    path('join/careers/', views.hiring, name='hiring'),
+    path('locations/<str:shopname>/', views.shop, name='shops'),
     path('about/impact/', views.impact, name='impact'),
     path('about/people/', views.people, name='people'),
-    path('about/hiring/', views.hiring, name='hiring'),
+    path('about/programs/', views.programs, name='programs'),
     
 ]
